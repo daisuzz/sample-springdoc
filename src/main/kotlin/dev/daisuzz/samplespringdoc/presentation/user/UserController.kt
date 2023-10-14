40c21874e747108c7da499c0c96de06b2aa33323
daisuzz <daisaku.rugby@gmail.com>
2023-10-14 12:59:08 +0900

    Add user create api

--- src/main/kotlin/dev/daisuzz/samplespringdoc/presentation/user/UserController.kt
+++ src/main/kotlin/dev/daisuzz/samplespringdoc/presentation/user/UserController.kt
@@ -2,8 +2,12 @@ package dev.daisuzz.samplespringdoc.presentation.user
 
 import io.swagger.v3.oas.annotations.Operation
 import io.swagger.v3.oas.annotations.tags.Tag
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.DeleteMapping
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
@@ -14,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController
 class UserController {
 
     @GetMapping
-    @Operation(summary = "ユーザ一覧取得API", description = "ユーザ一覧取得API")
+    @Operation(summary = "ユーザ一覧取得API", description = "全ユーザを取得する。")
     fun getUsers(): List<UserResponse> {
         return listOf(
             UserResponse(
@@ -31,8 +35,20 @@ class UserController {
     }
 
     @GetMapping("{userId}")
-    @Operation(summary = "ユーザ取得API", description = "ユーザ取得API")
+    @Operation(summary = "ユーザ取得API", description = "指定されたユーザIDに紐づくユーザを取得する。")
     fun getUser(@PathVariable userId: String): UserResponse {
         return UserResponse("1", "Alice", 20)
+    }
+
+    @PostMapping
+    @Operation(summary = "ユーザ登録API", description = "ユーザを登録する。")
+    fun createUser(@RequestBody userRequest: UserRequest): ResponseEntity<String> {
+        return ResponseEntity.ok().build()
+    }
+
+    @DeleteMapping("{userId}")
+    @Operation(summary = "ユーザ削除API", description = "指定されたユーザIDを削除する。")
+    fun deleteUser(@PathVariable userId: String): ResponseEntity<String> {
+        return ResponseEntity.ok().build()
     }
 }
---
+++ src/main/kotlin/dev/daisuzz/samplespringdoc/presentation/user/UserRequest.kt
@@ -0,0 +1,6 @@
+package dev.daisuzz.samplespringdoc.presentation.user
+
+data class UserRequest(
+    val name: String,
+    val age: Int,
+)
