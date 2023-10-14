be3b5cb38e1d9838d4a155d20c8d6954f3dce2d4
daisuzz <daisaku.rugby@gmail.com>
2023-10-14 13:09:04 +0900

    Add ApiResponse docs in User API

--- src/main/kotlin/dev/daisuzz/samplespringdoc/presentation/user/UserController.kt
+++ src/main/kotlin/dev/daisuzz/samplespringdoc/presentation/user/UserController.kt
@@ -1,6 +1,7 @@
 package dev.daisuzz.samplespringdoc.presentation.user
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.DeleteMapping
@@ -35,19 +36,40 @@ class UserController {
     }
 
     @GetMapping("{userId}")
-    @Operation(summary = "ユーザ取得API", description = "指定されたユーザIDに紐づくユーザを取得する。")
+    @Operation(
+        summary = "ユーザ取得API",
+        description = "指定されたユーザIDに紐づくユーザを取得する。",
+        responses = [
+            ApiResponse(responseCode = "200", description = "ユーザを取得できた場合"),
+            ApiResponse(responseCode = "404", description = "存在しないユーザが指定された場合")
+        ]
+    )
     fun getUser(@PathVariable userId: String): UserResponse {
         return UserResponse("1", "Alice", 20)
     }
 
     @PostMapping
-    @Operation(summary = "ユーザ登録API", description = "ユーザを登録する。")
+    @Operation(
+        summary = "ユーザ登録API",
+        description = "ユーザを登録する。",
+        responses = [
+            ApiResponse(responseCode = "200", description = "ユーザを取得できた場合"),
+            ApiResponse(responseCode = "400", description = "リクエストに含まれるユーザ情報に不備がある場合")
+        ]
+    )
     fun createUser(@RequestBody userRequest: UserRequest): ResponseEntity<String> {
         return ResponseEntity.ok().build()
     }
 
     @DeleteMapping("{userId}")
-    @Operation(summary = "ユーザ削除API", description = "指定されたユーザIDを削除する。")
+    @Operation(
+        summary = "ユーザ削除API",
+        description = "指定されたユーザIDを削除する。",
+        responses = [
+            ApiResponse(responseCode = "200", description = "ユーザを削除できた場合"),
+            ApiResponse(responseCode = "404", description = "存在しないユーザが指定された場合")
+        ]
+    )
     fun deleteUser(@PathVariable userId: String): ResponseEntity<String> {
         return ResponseEntity.ok().build()
     }
