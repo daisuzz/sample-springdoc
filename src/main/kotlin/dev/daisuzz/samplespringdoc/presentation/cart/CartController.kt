7f08c9e7dd081cda4d697a14c1ae87d5a703ed18
daisuzz <daisaku.rugby@gmail.com>
2023-10-14 11:50:02 +0900

    add tag annotation

--- src/main/kotlin/dev/daisuzz/samplespringdoc/presentation/cart/CartController.kt
+++ src/main/kotlin/dev/daisuzz/samplespringdoc/presentation/cart/CartController.kt
@@ -1,6 +1,7 @@
 package dev.daisuzz.samplespringdoc.presentation.cart
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("carts")
 @Suppress("MagicNumber", "UnusedParameter")
+@Tag(name = "カートAPI", description = "カートに関する操作を行うAPI")
 class CartController {
 
     @GetMapping
--- src/main/kotlin/dev/daisuzz/samplespringdoc/presentation/user/UserController.kt
+++ src/main/kotlin/dev/daisuzz/samplespringdoc/presentation/user/UserController.kt
@@ -1,6 +1,7 @@
 package dev.daisuzz.samplespringdoc.presentation.user
 
 import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.web.bind.annotation.RequestMapping
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController
 @RestController
 @RequestMapping("users")
 @Suppress("MagicNumber", "UnusedParameter")
+@Tag(name = "ユーザAPI", description = "ユーザに関する操作を行うAPI")
 class UserController {
 
     @GetMapping
