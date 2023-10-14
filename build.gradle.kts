c243fa926756c590e20e46b79d66265d2382d474
daisuzz <daisaku.rugby@gmail.com>
2023-10-14 15:50:41 +0900

    Add gradle task that outputs OpenAPI definition

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,6 +5,7 @@ plugins {
     id("io.spring.dependency-management") version "1.1.3"
     kotlin("jvm") version "1.8.22"
     kotlin("plugin.spring") version "1.8.22"
+    id("org.springdoc.openapi-gradle-plugin") version "1.7.0"
 }
 
 group = "dev.daisuzz"
