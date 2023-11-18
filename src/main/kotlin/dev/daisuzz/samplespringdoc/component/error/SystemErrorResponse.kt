99ae66849cb67a55a1a3e2233006e13b5c7a11fa
daisuzz <daisaku.rugby@gmail.com>
2023-11-18 12:02:43 +0900

    fix

--- src/main/kotlin/dev/daisuzz/samplespringdoc/component/error/SystemErrorResponse.kt
+++ src/main/kotlin/dev/daisuzz/samplespringdoc/component/error/SystemErrorResponse.kt
@@ -3,9 +3,9 @@ package dev.daisuzz.samplespringdoc.component.error
 import io.swagger.v3.oas.annotations.media.Schema
 
 data class SystemErrorResponse(
-    @Schema(description = "エラーメッセージ", example = "入力エラー")
+    @Schema(description = "エラーメッセージ", example = "システムエラー")
     val message: String,
 
-    @Schema(description = "エラーコード", example = "ERROR_00001")
+    @Schema(description = "エラーコード", example = "ERROR_00002")
     val errorCode: String,
 )
